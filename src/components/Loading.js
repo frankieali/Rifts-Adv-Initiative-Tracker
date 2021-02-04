8b7cc9599b2f2d3a1b3c8c678cd0f1828ae1eb2f
alifr <frankieali4@gmail.com>
2021-02-04 08:35:45 -0500

    restyle loading indicator

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { CircularProgress } from '@material-ui/core'
 
 export default (props) => (
-  <div style={{display:'flex', flexDirection:'row', justifyContent:'center'}}>
-    <img src={`/assets/images/lava-lamp.svg`} alt="loading indicator" width="250px" style={{width:250,height:250,margin:'auto 0'}} />
-    { props.message && <span>{props.message}</span>}
+  <div style={{display:'flex', flexDirection:'row', justifyContent:'center', alignItems:'center', height:'70vh'}}>
+    {/* <img src={`/assets/images/lava-lamp.svg`} alt="loading indicator" width="250px" style={{width:250,height:250,margin:'auto 0'}} />
+    { props.message && <span>{props.message}</span>} */}
+    <CircularProgress size='9rem' />
   </div>
 )
